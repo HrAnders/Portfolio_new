db3a2fb6d3964edf65247e634b642a9992803852
Stefan Anders <stefananders@hotmail.de>
2023-10-18 22:01:15 +0200

    added header comp, added fonts

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
     HeaderComponent
   ],
   imports: [
