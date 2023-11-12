de2a9b9c70119ecaa4793723c868e9bfce9d7290
Stefan Anders <stefananders@hotmail.de>
2023-11-12 14:21:01 +0100

    edited form field: checkbox style

--- portfolio/src/app/contact-form/contact-form.component.ts
+++ portfolio/src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, Input, OnInit, ViewChild,  Renderer2 } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+  Renderer2,
+} from '@angular/core';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
@@ -27,46 +34,59 @@ export class ContactFormComponent implements OnInit {
   msgValid: boolean = true;
   msgInput: string = '';
 
- 
   constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {}
 
   async sendMail() {
     try {
-      console.log(
-        'Name: ' +
-          this.nameField.nativeElement.value +
-          ' Message: ' +
-          this.messageField.nativeElement.value
-      );
-      let nameField = this.nameField.nativeElement;
-      let mailField = this.mailField.nativeElement;
-      let messageField = this.messageField.nativeElement;
-      let sendButton = this.sendButton.nativeElement;
+      let inputElements = this.getInputElements();
+      this.enableInputElements(inputElements);
 
-      nameField.disabled = true;
-      messageField.disabled = true;
-      sendButton.disabled = true;
+      //await fetch(
+      //  'https://stefan-anders.developerakademie.net/Formular/send_mail.php',
+      //  {
+      //    method: 'POST',
+      //    body: this.setFormdata(inputElements)
+      //    ,
+      //  }
+      //),
 
-      let formData = new FormData();
-      formData.append('name', nameField.value);
-      formData.append('mail', mailField.value);
-      formData.append('message', messageField.value);
+     this.disableInputElements(inputElements);
 
-      await fetch(
-        'https://stefan-anders.developerakademie.net/Formular/send_mail.php',
-        {
-          method: 'POST',
-          body: formData,
-        }
-      ),
-      nameField.disabled = false;
-      messageField.disabled = false;
-      sendButton.disabled = false;
     } catch (error) {
       console.log(error);
     }
+  }
+
+  getInputElements(){
+    let nameField = this.nameField.nativeElement;
+    let mailField = this.mailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let sendButton = this.sendButton.nativeElement;
+    return {nameField, mailField, messageField, sendButton}
+  }
+
+  disableInputElements(inputElements: any){
+    inputElements['nameField'].disabled = true;
+    inputElements['mailField'].disabled = true;
+    inputElements['messageField'].disabled = true;
+    inputElements['sendButton'].disabled = true;
+  }
+
+  enableInputElements(inputElements: any){
+    inputElements['nameField'].disabled = false;
+    inputElements['mailField'].disabled = false;
+    inputElements['messageField'].disabled = false;
+    inputElements['sendButton'].disabled = false;
+  }
+
+  setFormdata(inputElements:any){
+    let formData = new FormData();
+    formData.append('name', inputElements['nameField'].value);
+    formData.append('mail', inputElements['mailField'].value);
+    formData.append('message', inputElements['messageField'].value);
+    return formData
   }
 
   checkName() {
@@ -79,15 +99,29 @@ export class ContactFormComponent implements OnInit {
 
   checkMail() {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  
+
     if (!emailPattern.test(this.mailInput)) {
       this.mailError.nativeElement.hidden = false;
-      this.renderer.setProperty(this.mailError.nativeElement, 'innerHTML', 'Please enter a valid email address');
+      this.renderer.setProperty(
+        this.mailError.nativeElement,
+        'innerHTML',
+        'Please enter a valid email address.'
+      );
     } else {
       this.mailError.nativeElement.hidden = true;
     }
   }
-  
 
-   
+  checkMessage() {
+    if (this.msgInput.length > 10) {
+      this.msgError.nativeElement.hidden = true;
+    } else {
+      this.msgError.nativeElement.hidden = false;
+      this.renderer.setProperty(
+        this.msgError.nativeElement,
+        'innerHTML',
+        'Your message is too short.'
+      );
+    }
+  }
 }
